ccf279418ef8b3923946c496fbb453fb150bdf7c
Stephen DiPietro <stevedipietro97@gmail.com>
2021-09-07 19:47:13 -0400

    Added csrf token and updated search component with axios request

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -7,7 +7,15 @@ class Search extends Component {
 
     render(){
         return(
-            <div>Search goes here</div>
+            <div>
+                <h1>Cryptocurrency Potoflio Calculator</h1>
+                <form>
+                    <div className="form-group">
+                        <label>Search for a Currency:</label><br/>
+                        <input onChange={this.props.handleChange} autoComplete="off" type="text" name="name" placeholder="Ex: Bitcoin, Ethereum, Dogecoin..." value={this.props.name} className="field"></input>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
