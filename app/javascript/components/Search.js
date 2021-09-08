3d34501fe0387d665ed58f1ce229788c2ffc67cd
Stephen DiPietro <stevedipietro97@gmail.com>
2021-09-08 01:43:11 -0400

    Added cryto-symbols to search results

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -6,7 +6,7 @@ class Search extends Component {
     }
 
     render(){
-        const searchResults = this.props.searchResults.map( curr => <li key={curr.id}>{curr.name}</li>)
+        const searchResults = this.props.searchResults.map( curr => <li key={curr.id} data-id={curr.id} onClick={this.props.handleSelect} className="currency-list-item"><a href="#" className="currency"><span>{curr.name}</span> <span>{curr.currency_symbol}</span></a></li>)
         return(
             <div>
                 <h1>Cryptocurrency Potoflio Calculator</h1>
